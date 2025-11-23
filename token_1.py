3546e24d5bf881f4c69d438337abdfcdce4a8047
MBOCKE <mbockegabriel@gmail.com>
2025-11-23 16:44:24 +0100

    Add State, NFA, and Token classes

--- token_1.py
+++ token_1.py
@@ -4,4 +4,9 @@ from typing import List, Optional, Set
 from token_type import TokenType
 
 class Token:
-    pass
+    def __init__(self, type: TokenType, value: Optional[str] = None):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        return f"Token({self.type}, {self.value!r})"
